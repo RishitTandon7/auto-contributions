5f3093c12579f0dd97224042942cfd8a044dd9b3
RishitTandon7 <142263549+RishitTandon7@users.noreply.github.com>
2026-03-27 19:36:26 +0000

    Add python tutorial [Mar 27, 2026]

---
+++ tutorials/2026/03/python_27_mar_489.py
@@ -0,0 +1,9 @@
+# Trie Data Structure Implementation in Python
+=====================================================
+
+## Introduction
+------------
+
+A trie is a data structure that stores a collection of strings in a way that allows for efficient retrieval and insertion of words.
+
+## Implementation
